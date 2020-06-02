a72d24e10be0f13ce6711610e43d7001ca221f34
Claudio Luciano <claudio_luciano@live.com>
2020-06-01 22:17:11 -0400

    Update alpine-brew-fish.sh

--- scripts/alpine-brew-fish.sh
+++ scripts/alpine-brew-fish.sh
@@ -1,8 +1,4 @@
 #!/bin/ash
-#-------------------------------------------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
-#-------------------------------------------------------------------------------------------------------------
 
 # Syntax: ./common-alpine.sh <install zsh flag> <username> <user UID> <user GID> 
 
